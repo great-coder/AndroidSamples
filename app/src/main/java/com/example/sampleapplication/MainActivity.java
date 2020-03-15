62697289380883c996587a40e0cbd7d250eb39cf
Coder the Great <m_dehghan9112@outlook.com>
2020-03-15 21:43:20 +0330

    Update main activity to add Video Player module...

--- app/src/main/java/com/example/sampleapplication/MainActivity.java
+++ app/src/main/java/com/example/sampleapplication/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sampleapplication.modules.AnimationActivity;
 import com.example.sampleapplication.modules.GameActivity;
+import com.example.sampleapplication.modules.VideoPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void ClickGame(View view) {
         Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void ClickVideoPlayer(View view) {
+        Intent intent = new Intent(this, VideoPlayerActivity.class);
         startActivity(intent);
     }
 }
