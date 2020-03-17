9c71893d54689a26c2ae17c7e21fccf76966f2a6
Coder the Great <m_dehghan9112@outlook.com>
2020-03-17 13:59:41 +0330

    Module Timer App ...

--- app/src/main/java/com/example/sampleapplication/MainActivity.java
+++ app/src/main/java/com/example/sampleapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sampleapplication.modules.AnimationActivity;
+import com.example.sampleapplication.modules.AppTimerActivity;
 import com.example.sampleapplication.modules.AudioControlActivity;
 import com.example.sampleapplication.modules.GameActivity;
 import com.example.sampleapplication.modules.ListViewActivity;
@@ -39,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case "MODULE_5":
                 intent = new Intent(this, ListViewActivity.class);
+                break;
+            case "MODULE_6":
+                intent = new Intent(this, AppTimerActivity.class);
                 break;
             default:
                 Toast.makeText(this, "Unsupported command!", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/sampleapplication/modules/AppTimerActivity.java
@@ -0,0 +1,97 @@
+package com.example.sampleapplication.modules;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sampleapplication.R;
+
+public class AppTimerActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button controllerBtn;
+    CountDownTimer countDownTimer;
+    boolean isCounterActive = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_timer);
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
+        controllerBtn = findViewById(R.id.controllerBtn);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //
+            }
+        });
+    }
+
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+        String secondString = Integer.toString(seconds);
+        String minuteString = Integer.toString(minutes);
+        if (seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+        if (minutes <= 9) {
+            minuteString = "0" + minuteString;
+        }
+        timerTextView.setText(minuteString + ":" + secondString);
+    }
+
+    public void resetTimer() {
+        timerTextView.setText("00:30");
+        timerSeekBar.setProgress(30);
+        isCounterActive = false;
+        timerSeekBar.setEnabled(true);
+        controllerBtn.setText("Go!");
+        countDownTimer.cancel();
+    }
+
+    public void controlTimer(View view) {
+        if (!isCounterActive) {
+            isCounterActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerBtn.setText("Stop");
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    resetTimer();
+                    // TODO: you can play a sound here to notify user that timer is done
+                    Toast.makeText(AppTimerActivity.this, "Timer has finished!", Toast.LENGTH_SHORT).show();
+                }
+            }.start();
+        } else {
+            resetTimer();
+        }
+    }
+}
